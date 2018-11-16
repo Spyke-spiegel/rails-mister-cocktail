0367ae0038072131ba9482e3fd4d3afd1e8d4a65
Florent Ayot <ayotflorent@gmail.com>
2018-11-16 13:08:17 +0800

    making the controller working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'cocktail/ingredient'
+  # get 'cocktail/dose'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :cocktails
+  # resources :ingredients
+  # resources :doses
+  resources :cocktails do
+    resources :doses
+  end
+
+  root to: "cocktails#index"
 end
